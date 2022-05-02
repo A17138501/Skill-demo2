d58727c769183d6e94a93ad9c2b3876d4bb598e0
A17138501 <97008935+A17138501@users.noreply.github.com>
2022-05-01 23:19:20 -0700

    finish code

---
+++ SkillDemotest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class SkillDemotest {
+    
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo2.add(4,5));
+    }
+}
+}
